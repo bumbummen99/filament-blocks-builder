77f6ab089cdb676c183f6eb071aa572fb6e2cedb
Patrick Henninger <privat@skyraptor.eu>
2024-11-24 07:43:45 +0100

    [Add] WIP Basic Browser test to verify BlocksInput interactivity

---
+++ tests/Browser/BlocksInputTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\SkyRaptor\FilamentBlocksBuilder\Feature;
+
+class BlocksInputTest extends TestCase
+{
+    /**
+     * This test is intended to ensure that nested BlocksInput
+     * instances do work and interact as intended.
+     */
+    function test_nested_builders_interaction()
+    {
+        $this->assertTrue(true);
+    }
+}
