3184754411949232121bf3d6ebab7c084cd48951
Patrick Henninger <privat@skyraptor.eu>
2024-11-24 07:36:11 +0100

    [Change] Use Concern over Attribute to stay in line with other Concerns

--- tests/Concerns/RequiresApplicationEnvironment.php
+++ tests/Concerns/RequiresApplicationEnvironment.php
@@ -3,22 +3,37 @@
 namespace Tests\SkyRaptor\FilamentBlocksBuilder\Concerns;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Orchestra\Testbench\Attributes\WithMigration;
+use Orchestra\Testbench\Concerns\WithLaravelMigrations;
 use Orchestra\Testbench\Concerns\WithWorkbench;
-use PHPUnit\Framework\Attributes\Before;
 use Workbench\App\Models\User;
 
-#[WithMigration]
+/**
+ * This Concern is intended to initialize the basic
+ * application environment for more in-depth tests.
+ */
 trait RequiresApplicationEnvironment
 {
-    /* Use Orchestral Testbench's Workbench environment */
+    /**
+     * Use Orchestral Testbench's Workbench & load testbench.yaml.
+     * 
+     * @see https://packages.tools/testbench.html#autoloading-using-testbench-yaml
+     */
     use WithWorkbench;
 
-    /* Ensure the databse is migrated & fresh before each test */
+    /**
+     * Run Laravel Migrations defined in Orchestral Testbench's Workbench environment.
+     */
+    use WithLaravelMigrations;
+
+    /**
+     * Ensure the databse is migrated & fresh before each test.
+     * 
+     * @see https://laravel.com/docs/11.x/database-testing#resetting-the-database-after-each-test
+     */
     use RefreshDatabase;
 
     /**
-     * The default testing User
+     * The default testing User Fixture
      */
     protected User $user;
 
